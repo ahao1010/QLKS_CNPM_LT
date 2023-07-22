e5253f8c0bf272fd14f374704d3348a46d081a64
DizisNak <khangnguyen456vn@gmail.com>
2023-07-22 20:08:25 +0700

    Add project files.

---
+++ QLKS_CNPM_LT/Controllers/PhongController.cs
@@ -0,0 +1,19 @@
+﻿using QLKS_CNPM_LT.Models.Function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLKS_CNPM_LT.Controllers
+{
+    public class PhongController : Controller
+    {
+        // GET: Phong
+        public ActionResult DSPhong()
+        {
+            var func_phong = new Phong_Func();
+            return View(func_phong.toanBoPhong());
+        }
+    }
+}
